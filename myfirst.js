3ac1a0a5071e50adf1dd473d2e824aa0dee62cf6
Sara Pellnor <sara.ahlquist@hotmail.com>
2022-12-12 18:32:24 +0100

    fgjkgfyk

---
+++ myfirst.js
@@ -0,0 +1,11 @@
+function timeOutfunction(myFunction) {
+    console.log("1")
+    setTimeout (myFunction, 2000)
+    console.log("2")
+  }
+  
+  function myFunction () {
+    console.log("3")
+  }
+  
+  timeOutfunction(myFunction)
