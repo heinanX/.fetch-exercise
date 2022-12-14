a426ee4671e1eb19406e722e32f621fdee59a42e
Sara Pellnor <sara.ahlquist@hotmail.com>
2022-12-14 12:22:14 +0100

    finish nr 2

--- myfirst.js
+++ myfirst.js
@@ -1,11 +1,36 @@
-function timeOutfunction(myFunction) {
-    console.log("1")
-    setTimeout (myFunction, 2000)
-    console.log("2")
-  }
+
+
+
+
+// function timeOutfunction(myFunction) {
+//   console.log("1")
+//   setTimeout (myFunction, 2000)
+//   console.log("2")
+// }
+
+// function myFunction () {
+//   console.log("3")
+// }
+
+// timeOutfunction(myFunction)
+
+
+fetch("http://sholiday.faboul.se/dagar/v2.1/2006")
+  .then((response) => response.json())
+  .then((data) => writeOutInfo(data));
   
-  function myFunction () {
-    console.log("3")
-  }
-  
-  timeOutfunction(myFunction)
+
+function writeOutInfo (data) {
+  let days = data.dagar;
+
+  days.forEach(object => {
+    let holiday = object.helgdag
+    
+    if (holiday) {
+      let p = document.createElement("p")
+      p.innerHTML =object.datum + ": " + holiday
+      document.querySelector("body").appendChild(p)
+      console.log(object)
+    }
+  });
+}
