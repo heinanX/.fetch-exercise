92cd16d45f822190d3b0d6f11164bf5e35a87348
Sara Pellnor <sara.ahlquist@hotmail.com>
2022-12-16 11:33:18 +0100

    now finished

--- goldStar.js
+++ goldStar.js
@@ -6,26 +6,32 @@ function getDates(year, times) {
    if (dif < 2022) {
     for (dif; dif<=year;  dif++) {
         array.push(dif)
-   }
+        }
+    }
    array.forEach(object => {
-    myFetch(object)
-   })
-}
-   
-function myFetch(object) {
-    Promise.all (fetch ("http://sholiday.faboul.se/dagar/v2.1/"+ object))
+    // myFetch(object)
+    Promise.all ([fetch ("http://sholiday.faboul.se/dagar/v2.1/"+ object)])
     .then((responses) => Promise.all(responses.map((response) => response.json())))
     .then((data) => myFunction(data))
+    })
+    
 }
-};
+
+   
+// function myFetch(object) {
+//     Promise.all (fetch ("http://sholiday.faboul.se/dagar/v2.1/"+ object))
+//     .then((responses) => Promise.all(responses.map((response) => response.json())))
+//     .then((data) => myFunction(data))
+// }
+// };
 
 function myFunction(data) {
     const body = document.querySelector("body")
     for (const year of [data]) {
-        for (const years of [year]) {
+        for (const years of year) {
             for (const days of years.dagar){
                 let p = document.createElement("p")
-                p.innerHTML = days.datum
+                p.innerHTML = days.datum + ", "
                 body.append(p)
             }
         }
